0b8746e138baf862eb7618e08febbaebf215dc9f
michael Tawiah Sowah <michael.t.sowah@gmail.com>
2026-09-23 11:17:10 +0000

    Release v1.0.0-beta.54 — Developer Preview

---
+++ resources/admin/assets/roles-Bc6coUcQ.js
@@ -0,0 +1,1 @@
+import{H as e,g as t,w as n}from"./vue.runtime.esm-bundler-DHLDW_US.js";import{t as r}from"./roles-CKjqM7R4.js";var i=n({__name:`roles`,setup(n){return(n,i)=>(e(),t(r))}});export{i as default};
